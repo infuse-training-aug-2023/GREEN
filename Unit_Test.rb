eb63bb8f3249ec5609195f1ba89257e08a72c1b4
Angelo <angelogoves@gmail.com>
2023-08-28 23:10:16 +0530

    made a static variable for url

--- Unit_Test.rb
+++ Unit_Test.rb
@@ -3,6 +3,8 @@ require "test/unit"
 require_relative "Selenium_Wrapper"
 
 class TestAverage < Test::Unit::TestCase
+  @@url = "http://localhost:5500/test/index.html"
+
   def test_selenium_wrapper_object
     test_creation_selenium_wrapper = SeleniumWrapper.new
     assert test_creation_selenium_wrapper.instance_of? SeleniumWrapper
@@ -10,7 +12,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_open_website
     test_creation_selenium = SeleniumWrapper.new
-    assert_equal(nil, test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html"))
+    assert_equal(nil, test_creation_selenium.open_website(@@url))
     test_creation_selenium.quit
   end
 
@@ -25,14 +27,14 @@ class TestAverage < Test::Unit::TestCase
 
   def test_set_wait
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     assert_not_nil(test_creation_selenium.set_wait(10))
     test_creation_selenium.quit
   end
 
   def test_set_wait_failed
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     assert_raise(RuntimeError) do
       test_creation_selenium.set_wait("10")
     end
@@ -41,7 +43,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_send_keys
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     assert_equal(1, test_creation_selenium.send_keys(:id, "username", "usr"))
     test_creation_selenium.quit
@@ -49,7 +51,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_send_keys_failed
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     assert_raise(RuntimeError) do
       test_creation_selenium.send_keys(:id, "usernaame", "usr")
@@ -59,7 +61,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_select_options
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     assert_equal(1, test_creation_selenium.select_options(:name, "role", :value, "admin"))
     test_creation_selenium.quit
@@ -67,7 +69,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_select_options_failed
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     assert_raise(RuntimeError) do
       test_creation_selenium.select_options(:name, "rolle", :value, "admin")
@@ -77,7 +79,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_click
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     test_creation_selenium.send_keys(:id, "username", "usr")
     test_creation_selenium.send_keys(:id, "password", "pass")
@@ -90,7 +92,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_click_failed
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     test_creation_selenium.send_keys(:id, "username", "usr")
     test_creation_selenium.send_keys(:id, "password", "pass")
@@ -103,7 +105,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_get_element
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     test_creation_selenium.send_keys(:id, "username", "usr")
     test_creation_selenium.send_keys(:id, "password", "pass")
@@ -117,7 +119,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_get_element_failed
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     test_creation_selenium.set_wait(10)
     test_creation_selenium.send_keys(:id, "username", "usr")
     test_creation_selenium.send_keys(:id, "password", "pass")
@@ -131,7 +133,7 @@ class TestAverage < Test::Unit::TestCase
 
   def test_quit
     test_creation_selenium = SeleniumWrapper.new
-    test_creation_selenium.open_website("http://127.0.0.1:5500/test/index.html")
+    test_creation_selenium.open_website(@@url)
     assert_equal(nil, test_creation_selenium.quit())
   end
 end
