b2904a1cb032f469974dbaab4753ea3b73dd7354
Angelo <angelogoves@gmail.com>
2023-08-28 23:11:51 +0530

    resolved merge conflicts

--- Selenium_Wrapper.rb
+++ Selenium_Wrapper.rb
@@ -50,38 +50,6 @@ class SeleniumWrapper
     end
   end
 
-  def send_keys(selector, value, key_strocks)
-    begin
-      element = @driver.find_element(selector, value)
-      element.send_keys key_strocks
-      return 1
-    rescue => exception
-      raise "element not found"
-      return -1
-    end
-  end
-
-  def select_options(selector, value, how, what)
-    begin
-      element = Selenium::WebDriver::Support::Select.new(@driver.find_element(selector, value))
-      element.select_by(how, what)
-      return 1
-    rescue => exception
-      raise "element not found"
-      return 1
-    end
-  end
-
-  def get_element(selector, value)
-    begin
-      element = @driver.find_element(selector, value)
-      return element
-    rescue => exception
-      raise "element not found"
-      return nil
-    end
-  end
-
   def select_options(selector, value, how, what)
     begin
       element = Selenium::WebDriver::Support::Select.new(@driver.find_element(selector, value))
