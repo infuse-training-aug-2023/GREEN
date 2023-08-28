7e05bdbfa58ddeab086bc200ba1b220828c54661
Angelo <angelogoves@gmail.com>
2023-08-28 12:36:02 +0530

    Added username field validation

--- test/script.js
+++ test/script.js
@@ -1,11 +1,16 @@
 function displayDetails() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var role = document.getElementById("role").value;
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	var role = document.getElementById("role").value;
 
-    document.getElementById("displayUsername").textContent = username;
-    document.getElementById("displayPassword").textContent = password;
-    document.getElementById("displayRole").textContent = role;
+	if (username === "") {
+		alert("Please enter a username.");
+		return;
+	}
 
-    document.getElementById("details").style.display = "block";
+	document.getElementById("displayUsername").textContent = username;
+	document.getElementById("displayPassword").textContent = password;
+	document.getElementById("displayRole").textContent = role;
+
+	document.getElementById("details").style.display = "block";
 }
