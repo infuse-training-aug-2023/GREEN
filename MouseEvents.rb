9517ab3799fbc61a99d08ec0740424df09d12c6a
Angelo <angelogoves@gmail.com>
2023-08-29 13:14:07 +0530

    Added mouse events

---
+++ MouseEvents.rb
@@ -0,0 +1,30 @@
+require_relative "Driver"
+
+class MouseEvents < Driver
+  def initialize(driver, wait)
+    @driver = driver
+    @wait = wait
+  end
+
+  def click(selector, value)
+    begin
+      element = @driver.find_element(selector, value)
+      element.click
+      return 1
+    rescue => exception
+      raise "element not found"
+      return -1
+    end
+  end
+
+  def select_options(selector, value, how, what)
+    begin
+      element = Selenium::WebDriver::Support::Select.new(@driver.find_element(selector, value))
+      element.select_by(how, what)
+      return 1
+    rescue => exception
+      raise "element not found"
+      return 1
+    end
+  end
+end
