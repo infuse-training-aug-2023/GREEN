2f1efe36672fff1f30f64adf2f906fa368ed3ba3
ANish Naik <naikanish13@gmail.com>
2023-08-29 12:59:29 +0530

    resolved merge conflicts

--- Driver.rb
+++ Driver.rb
@@ -1,11 +1,37 @@
+require "selenium-webdriver"
+
+Selenium::WebDriver::Chrome::Service.driver_path = "./driver/chromedriver.exe"
+
 class Driver
   attr_accessor :driver
+  attr_accessor :wait
 
   def initialize
     @driver = Selenium::WebDriver.for :chrome
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  end
+
+  def set_wait(time_sec)
+    if (time_sec.is_a? Integer or time_sec.is_a? Float)
+      @wait = Selenium::WebDriver::Wait.new(:timeout => time_sec)
+    else
+      raise "Invalid timeout value"
+    end
+  end
+
+  def open_website(site_url)
+    @driver.get site_url
   end
 
   def quit
     @driver.quit
+  end
+
+  def get_driver
+    return @driver
+  end
+
+  def get_wait()
+    return @wait
   end
 end
