1742e1708acba1b55a85e77377f38cb8daa9f91e
Angelo <angelogoves@gmail.com>
2023-08-29 19:16:12 +0530

    Made event for selecting an option from the dropdown list

--- MouseEvents.rb
+++ MouseEvents.rb
@@ -6,25 +6,39 @@ class MouseEvents < Driver
     @wait = wait
   end
 
-  def click(selector, value)
+  def click(selector, selector_value)
     begin
-      element = @wait.until { @driver.find_element(selector, value) }
+      element = @wait.until { @driver.find_element(selector, selector_value) }
       element.click
       return 1
     rescue => exception
-      raise "element not found"
-      return -1
+      raise "Element not found: #{exception.message}"
     end
   end
 
-  def select_options(selector, value, how, what)
+  def select_options(selector, selector_value, how, what)
     begin
-      element = Selenium::WebDriver::Support::Select.new(wait.until { @driver.find_element(selector, value) })
+      element = Selenium::WebDriver::Support::Select.new(wait.until { @driver.find_element(selector, selector_value) })
       element.select_by(how, what)
       return 1
     rescue => exception
-      raise "element not found"
+      raise "Element not found: #{exception.message}"
+    end
+  end
+
+  def select_option_from_dropdown(selector, selector_value, attribute_value)
+    begin
+      dropdown = @driver.find_element(:id, "phone-code-select")
+      select_list = Selenium::WebDriver::Support::Select.new(dropdown)
+      select_list.options.each do |option|
+        if option.attribute("value") == attribute_value.to_s
+          option.click
+          break
+        end
+      end
       return 1
+    rescue => exception
+      raise "Element not found: #{exception.message}"
     end
   end
 end
