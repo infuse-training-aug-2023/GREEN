3ee691815e8c4f1d9b00fc8b44ed722fe2780793
ANish Naik <naikanish13@gmail.com>
2023-08-29 15:10:34 +0530

    added implicit wait

--- add_to_cart.rb
+++ add_to_cart.rb
@@ -5,17 +5,25 @@ class Add_TO_cart < Common_flow
     super
     @driver = get_driver
     @driver.set_wait(20)
+    @erros = 0
   end
 
   def get_shopping_items
-    # @driver.driver.driver.action.scroll_to(@driver.get_element(:xpath, "/html/body/div[1]/div/div[2]/div[2]/div[2]/section/div/div/div/div[2]/div/div/ul/li[1]")).perform
+    # if @erros < 5
+    #   begin
     @driver.click(:xpath, "/html/body/div[1]/div/div[2]/div[2]/div[2]/section/div/div/div/div[2]/div/div/ul/li[1]")
+    # rescue RuntimeError
+    #   @erros = @erros + 1
+    #   p "help"
+    #   get_shopping_items
+    # end
+    # end
   end
 end
 
 add = Add_TO_cart.new
 add.login
-sleep 5
+# sleep 5
 add.get_shopping_items
 sleep 5
 add.quit
